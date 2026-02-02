0638ffdc6d50440db5c244ab4d1ebb4e6a37ac28
purpledetailing <purpledetailing@gmail.com>
2026-02-02 13:26:03 -0500

    Update page.tsx

--- frontend/field-ui-next/src/app/new-job/page.tsx
+++ frontend/field-ui-next/src/app/new-job/page.tsx
@@ -1,8 +1,8 @@
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
-import NewJobClient from "./NewJobClient";
+import newjobclient from "./newjobclient";
 
 export default function Page() {
-  return <NewJobClient />;
+  return <newjobclient />;
 }
